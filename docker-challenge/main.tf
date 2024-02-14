560c0dfe533c68403503ece30255a21487d3c8e1
manil2010 <m.anilreddy.06@gmail.com>
2024-02-14 15:21:53 +0000

    workspaces

---
+++ docker-challenge/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.22.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "simplegoservice" {
+  name         = "registry.gitlab.com/alta3/simplegoservice"
+  keep_locally = true       // keep image after "destroy"
+}
+
+resource "docker_container" "simplegoservice" {
+  image = docker_image.simplegoservice.image_id
+  name  = "Alta3ResearchWebService"
+  ports {
+    internal = 9876
+    external = 5432
+  }
+}
